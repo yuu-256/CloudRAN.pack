a454c7bebeeb2eb43123997c4e325af2d70567c0
江崎雄大 <yudai@ezakiyuudainoiMac.local>
2024-11-09 16:43:47 +0900

    run

--- get1000.sh
+++ get1000.sh
@@ -7,7 +7,7 @@ start=`date +%s`
 echo "Generating 1000 cloud fields..."
 
 # Get cloud field at 1000 random seed values
-for i in {0..9}
+for i in {0..999}
 do
     # Get random seed value
     seed_value=$((RANDOM % 10000))
@@ -16,7 +16,7 @@ do
     python3 cloudgen.py "out_field/output_$i" $seed_value
 
     # Print progress
-    echo "Progress: $((i+1))/10"
+    echo "Progress: $((i+1))/1000"
 done
 
 echo "Done!"
